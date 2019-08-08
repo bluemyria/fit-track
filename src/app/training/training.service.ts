a13051427b264e735e2b3cb6715074c1a733ef19
bluemyria <bluemyria@users.noreply.github.com>
2019-08-08 17:16:03 +0300

    Complete & Cancel Exercise

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -5,6 +5,7 @@ import { Exercise } from './exercise.model';
 export class TrainingService {
   exerciseChanged = new Subject<Exercise>();
   private runningExercise: Exercise;
+  private exercises: Exercise[] = [];
 
   private availableExercises: Exercise[] = [
     { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
@@ -20,6 +21,23 @@ export class TrainingService {
   startExercise(selectedId: string) {
     this.runningExercise = this.availableExercises.find(ex => ex.id === selectedId);
     this.exerciseChanged.next({...this.runningExercise});
+  }
+
+  completeExercise() {
+    this.exercises.push({...this.runningExercise, date: new Date(), state: 'completed'});
+    this.runningExercise = null;
+    this.exerciseChanged.next(null);
+  }
+
+  cancelExercise(progress: number) {
+    this.exercises.push({
+      ...this.runningExercise,
+      duration: this.runningExercise.duration * (progress / 100),
+      calories: this.runningExercise.calories * (progress / 100),
+      date: new Date(),
+      state: 'cancelled'});
+    this.runningExercise = null;
+    this.exerciseChanged.next(null);
   }
 
   getRunningExercise() {
