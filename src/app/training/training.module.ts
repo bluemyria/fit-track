840650a8a65f3e91469339f6d050c81ad3267240
bluemyria <bluemyria@users.noreply.github.com>
2019-08-11 19:00:20 +0300

    Split Up Training Route (load eagerly)

--- src/app/training/training.module.ts
+++ src/app/training/training.module.ts
@@ -8,6 +8,7 @@ import { NewTrainingComponent } from './new-training/new-training.component';
 import { PastTrainingsComponent } from './past-trainings/past-trainings.component';
 
 import { SharedModule } from '../shared/shared.module';
+import { TrainingRoutingModule } from './training-routing.module';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { SharedModule } from '../shared/shared.module';
   ],
   imports: [
     SharedModule,
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    TrainingRoutingModule
   ],
   entryComponents: [StopTrainingComponent]
 })
